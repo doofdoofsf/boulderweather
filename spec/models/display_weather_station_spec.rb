71985b2e9d5ccb16630df1529fc8d621a8782bae
John Quinn <doofdoofsf@gmail.com>
2020-03-25 06:48:19 -0600

    small hash refactor

--- spec/models/display_weather_station_spec.rb
+++ spec/models/display_weather_station_spec.rb
@@ -6,8 +6,13 @@ RSpec.describe DisplayWeatherStation, :type => :model do
       @station_ids = DisplayWeatherStation.station_ids(:meso_west)
     end
 
-    it "gives a list of meso_west station ids" do
+    it 'gives a list of meso_west station ids' do
       expect(@station_ids).to include('BTAC2')
     end
-  end
+
+    it 'is a list of station hashes' do
+      sugarloaf = DisplayWeatherStation.stations[:sugarloaf][:meso_west]
+      expect(sugarloaf[:id]).to be('BTAC2')
+    end
+    end
 end
