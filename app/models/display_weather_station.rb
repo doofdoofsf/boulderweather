a6914774509d4f67827bcb1c091f69b0924f3fe9
John Quinn <doofdoofsf@gmail.com>
2020-03-25 10:13:36 -0600

    add clickable locations

--- app/models/display_weather_station.rb
+++ app/models/display_weather_station.rb
@@ -4,19 +4,27 @@ class DisplayWeatherStation
         meso_west: {
             sugarloaf: {
                 id: 'BTAC2',
-                location: '3 miles SW'
+                location: '3 miles SW',
+                latitude: 40.018056,
+                longitude: -105.361389
             },
             boulder: {
                 id: 'E7311',
-                location: '2 miles W'
+                location: '2 miles W',
+                latitude: 40.06518,
+                longitude: -105.32669
             },
             boulder_airport: {
                 id: 'KBDU',
-                location: '4 miles ESE'
+                location: '4 miles ESE',
+                latitude: 40.0394297,
+                longitude: -105.2258217
             },
             nederland: {
                 id: 'F1761',
-                location: '10 miles SW'
+                location: '10 miles SW',
+                latitude: 39.98733,
+                longitude: -105.44883
             }
         }
     }
