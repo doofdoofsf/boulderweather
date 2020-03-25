2df759971de2eb33192f921540672ad682525968
John Quinn <doofdoofsf@gmail.com>
2020-03-25 13:00:08 -0600

    light refactor

--- app/helpers/weather_station_helper.rb
+++ app/helpers/weather_station_helper.rb
@@ -7,4 +7,12 @@ module WeatherStationHelper
   def distance_of_time_in_minutes(from, to)
     ((to.to_time - from.to_time) / 60.0).round
   end
+
+  def google_maps_url(latitude, longitude)
+    "http://maps.google.com/maps?t=k&q=loc:#{latitude}+#{longitude}"
+  end
+
+  def meso_west_station_page_url(station_id)
+    "https://mesowest.utah.edu/cgi-bin/droman/meso_base.cgi?stn=#{station_id}"
+  end
 end
