ef9d8ff3fc8290939f4f73d4110713cf28fdfce3
John Quinn <doofdoofsf@gmail.com>
2020-03-25 07:29:29 -0600

    add location column

--- app/models/display_weather_station.rb
+++ app/models/display_weather_station.rb
@@ -3,22 +3,30 @@ class DisplayWeatherStation
     {
         meso_west: {
             sugarloaf: {
-                id: 'BTAC2'
+                id: 'BTAC2',
+                location: '3 miles SW'
             },
             boulder: {
-                id: 'E7311'
+                id: 'E7311',
+                location: '2 miles W'
             },
             boulder_airport: {
-                id: 'KBDU'
+                id: 'KBDU',
+                location: '4 miles ESE'
             },
             nederland: {
-                id: 'F1761'
+                id: 'F1761',
+                location: '10 miles SW'
             }
         }
     }
   end
 
+  def self.station_data(name)
+    self.stations.values.first.select{|n, _v| n == name}[name]
+  end
+
   def self.station_ids(provider)
-    DisplayWeatherStation.stations[provider].values.map{|h| h[:id]}
+    self.stations[provider].values.map{|h| h[:id]}
   end
 end
