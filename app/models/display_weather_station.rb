4c233cef6f07d88e3fc58e57390da1eceae5571a
John Quinn <doofdoofsf@gmail.com>
2020-05-12 15:46:50 -0600

    add elevations and titles

--- app/models/display_weather_station.rb
+++ app/models/display_weather_station.rb
@@ -4,38 +4,48 @@ class DisplayWeatherStation
 
         wunderground: {
             north_boulder_ridge: {
+                name: 'North Boulder Ridge',
                 id: 'KCOBOULD45',
                 location: 'above launch',
                 latitude: 40.05619,
-                longitude: -105.301689
+                longitude: -105.301689,
+                elevation: 6500
             },
             sugarloaf: {
+                name: "Sugarloaf",
                 id: 'KCOBOULD363',
                 location: '5 miles WSW',
                 latitude: 40.029228,
-                longitude: -105.396965
+                longitude: -105.396965,
+                elevation: 7693
             },
             winter_park: {
+                name: 'Winter Park',
                 id: 'KCOWINTE9',
                 location: '30 miles WSW',
                 latitude: 39.84396,
-                longitude: -105.783096
+                longitude: -105.783096,
+                elevation: 12060
             }
         },
         meso_west: {
             nederland: {
+                name: 'Nederland',
                 id: 'F1761',
                 location: '10 miles SW',
                 latitude: 39.98733,
-                longitude: -105.44883
+                longitude: -105.44883,
+                elevation: 8251
             }
         },
         mesa_lab: {
             ncar_mesa_lab: {
+                name: 'NCAR Mesa Lab',
                 id: 'ncar_mesa_lab',
                 location: '5 miles SSE',
                 latitude: 39.978285,
-                longitude: -105.275084
+                longitude: -105.275084,
+                elevation: 6184
             },
         }
     }
